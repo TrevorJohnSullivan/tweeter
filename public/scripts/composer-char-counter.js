39710ac4915c8c8385a0bdbff3c87bd28f6bdefe
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-03-16 17:00:11 -0230

    added code that counted the number of characters i entered into the tweet box and then changed color if it went below the threshold

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  console.log("Document is ready!");
+
+  $('.newer-tweet textarea').on('input', function () {
+    var remaining = 140 - $(this).val().length;
+    $(this).closest('form').find('.counter').text(remaining);
+
+    if (remaining < 0) {
+        $(this).closest('form').find('.counter').addClass('exceeded');
+    } else {
+        $(this).closest('form').find('.counter').removeClass('exceeded');
+    }
+});
+
+});
+
+
+
+
